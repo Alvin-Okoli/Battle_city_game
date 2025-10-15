dd2726928838dd0c2f0479e15a415a9d8ac8bf3f
Alvin <alvinokoli@gmail.com>
2025-10-15 16:50:44 +0100

    Added collision, movement and bullet physics

--- src/game setup/Scenes/LoadingScene.js
+++ src/game setup/Scenes/LoadingScene.js
@@ -4,13 +4,12 @@ export default class LoadingScene extends Phaser.Scene{
     }
 
     preload(){
-        console.log("Loading Scene");
-        console.log(this.game.config.width, this.game.config.height);
         this.load.image('bg', 'assets/wallpaper.jpeg');
         this.load.image('player', 'assets/player tank.png');
         this.load.image('block', 'assets/Red Brick Wall.jpeg');
         this.load.image('ironBlock', 'assets/iron_block.png');
         this.load.image('enemyTank', 'assets/enemy tank.png');
+        this.load.image('bullet', 'assets/bullet.png');
     }
 
     create(){
