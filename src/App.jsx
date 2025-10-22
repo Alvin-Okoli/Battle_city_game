a108ca501a0c8083811f8b6f32171697d4468507
Alvin <alvinokoli@gmail.com>
2025-10-22 14:51:28 +0100

    Added react router and logic for health and score

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import { useRef, useEffect } from 'react'
 import MakeGame from './Components/MakeGame'
-import Auth from './Components/AUth'
+import Auth from './Components/Auth'
 import "./App.css"
+import {createBrowserRouter, createRoutesFromChildren, Route, RouterProvider} from 'react-router-dom'
 
 function App() {  
-
+  const router = createBrowserRouter(
+    createRoutesFromChildren(
+      <>
+        <Route path='/' element={<Auth/>}/>
+        <Route path='/game' element={<MakeGame/>}/>
+      </>
+    )
+  )
 
   return (
-    <>
-      {/* <Auth/> */}
-      <MakeGame/>
-    </>
+    <RouterProvider router={router}/>
   )
 }
 
